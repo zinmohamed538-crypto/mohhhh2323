f79cba462f8c34602c14e285754bcbaa5ed6f078
zinmohamed538-crypto <zinmohamed538@gmail.com>
2026-05-07 11:25:19 +0200

    Update EmailValidationTest.java

--- src/test/java/EmailValidationTest.java
+++ src/test/java/EmailValidationTest.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EmailValidationTest {
+
+    @Test
+    public void testEmailValidation() {
+
+        String regex = "^[A-Za-z0-9.+]+@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*\\.[A-Za-z]{2,}$";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        assertTrue(pattern.matcher("test@example.com").matches());
+        assertTrue(pattern.matcher("test@stu.vistula.edu.pl").matches());
+
+        assertFalse(pattern.matcher("test@examplecom").matches());
+        assertFalse(pattern.matcher("test@vistula..pl").matches());
+    }
 }
