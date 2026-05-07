478447c9dd8e22904781e3c2af3f8b88f061778a
zinmohamed538-crypto <zinmohamed538@gmail.com>
2026-05-07 11:24:02 +0200

    Update StudentFilter.java

--- src/test/java/StudentFilter.java
+++ src/test/java/StudentFilter.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.regex.*;
 
 public class StudentFilter {
+
+    public static void filterGradeFive(String inputFile, String outputFile) throws IOException {
+
+        Pattern pattern = Pattern.compile(".*\\t5$");
+
+        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.matches()) {
+                writer.write(line);
+                writer.newLine();
+            }
+        }
+
+        reader.close();
+        writer.close();
+    }
 }
+
